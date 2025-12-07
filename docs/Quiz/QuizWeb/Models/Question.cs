ef935498c6934606dec33d0b6c1644ba4f5a42e5
BotKurek <p80lite@gmail.com>
2025-12-07 13:39:23 +0100

    Zmiana struktury kodu

--- docs/Quiz/QuizWeb/Models/Question.cs
+++
@@ -1,9 +0,0 @@
-namespace QuizWeb.Models
-{
-    public class Question
-    {
-        public int Id { get; set; } // Klucz główny bazy
-        public string Text { get; set; } = "";
-        public List<Answer> Answers { get; set; } = new List<Answer>();
-    }
-}
